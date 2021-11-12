1ad38dbc1407635c231685bc991cb6376068f5cf
Adrian S <89584348+GittyEvil@users.noreply.github.com>
2021-11-12 16:55:21 +0100

    Add files via upload

---
+++ m02u03.py
@@ -0,0 +1,25 @@
+print("jag handlade", 3 ,"pennor för", 12 ,"kr och", 1 ,"äpple för", 3.80 , "kr vilket totalt blev", 15.80 ,"kr" )
+
+print("Jag handlade"+ 3 +"pennor för"+ 12 + "kr och"+ 1 +"äpple för"+ 3.80 +"kr vilket totalt blev"+ 15.80 +"kr")
+
+
+äpple=3.80
+total=15.80
+a="Jag handlade"+ 3
+a+=" pennor för"+ 12
+a+=" Och "+ 1
+a+=" äpple för"+ äpple
+a+=" kr vilket totalt blev"+ total
+a+=" kr"
+
+print(a)
+
+frukt="äpple"
+item="pennor"
+
+kostnad1=input("vad kostade pennorna?")
+kostnad2=3.80
+kostnad3=15.80
+
+print(f"jag handlade 3 {item} för {kostnad1} kr och 1 {äpple} för {kostnad2} kr viket totalt blev {kostnad3} kr ")
+
